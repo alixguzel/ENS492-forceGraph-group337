3b25c73b849c6327fbaef7cc7f81f3ba76c72ee5
Ali Güzel <aliguzel.dev@gmail.com>
2023-04-15 17:53:32 +0300

    Kadir's code

--- forceGraph.js
+++ forceGraph.js
@@ -1,15 +1,55 @@
-// Random tree
-const N = 300;
-const gData = {
-  nodes: [...Array(N).keys()].map(i => ({ id: i })),
-  links: [...Array(N).keys()]
-    .filter(id => id)
-    .map(id => ({
-      source: id,
-      target: Math.round(Math.random() * (id-1))
-    }))
-};
+//const {dat} = require('dat.gui');
+const gui = new dat.GUI();
+const elem = document.getElementById('3d-graph');
+var x = 6;
+//var gData = JSON.parse(fs.readFileSync('./data.json'));
 
-const Graph = ForceGraph3D()
-  (document.getElementById('3d-graph'))
-    .graphData(gData);
+
+
+const Graph = ForceGraph3D()(elem)
+  .backgroundColor("#000")
+  .jsonUrl('./Data/3000sample.json')
+  .nodeAutoColorBy("group")
+  .nodeLabel(node => `${node.id}`)
+  //.nodeVisibility(node => node.group == 1)
+  .linkVisibility()
+
+/*if(x == 6){
+  Graph.onNodeClick(node => window.open(`${node.user}/${node.id}`, '_blank'));
+}
+else{
+  Graph.nodeVisibility(node => node.group == 1);
+}*/
+
+
+var parameters = {
+filter: 0
+};
+filterGroup = gui
+.add(parameters, "filter")
+.min(0)
+.max(2)
+.step(1)
+.name("Filter group")
+.listen();
+
+filterGroup.onFinishChange((newValue) => {
+// console.log(gData);
+const filteredNodes = gData.nodes.filter((n) => n.group == newValue);
+// console.log(filteredNodes);
+const filteredNodesIds = [];
+JSON.stringify(filteredNodes, (key, value) => {
+if (key === "id") filteredNodesIds.push(value);
+return value;
+});
+// console.log(filteredNodesIds);
+const filteredLinks = gData.links.filter(
+(e) =>
+  filteredNodesIds.includes(e.source.id) &
+  filteredNodesIds.includes(e.target.id)
+);
+// console.log(filteredLinks);
+const filteredData = { nodes: filteredNodes, links: filteredLinks };
+console.log(filteredData);
+Graph.graphData(filteredData);
+});
