aaa574a3b2c30af2f524835e540e5e9c34de30b3
Ali Güzel <aliguzel.dev@gmail.com>
2023-04-15 22:03:14 +0300

    new

--- forceGraph.js
+++ forceGraph.js
@@ -2,4 +2,5 @@ const Graph = ForceGraph3D()
       (document.getElementById('3d-graph'))
         .jsonUrl('../Data/Sample/sampleUnzip.json')
         .nodeLabel('screen_name')
-        .nodeAutoColorBy('followers_count');
+        .nodeAutoColorBy('followers_count')
+        .onNodeClick(node => window.open(`https://twitter.com/${node.screen_name}`, '_blank'));
