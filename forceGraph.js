7363a32f1460ea60ee9b305e7ad0657b1f7d4481
Ali Güzel <aliguzel.dev@gmail.com>
2023-04-16 18:46:22 +0300

    Update forceGraph.js

--- forceGraph.js
+++ forceGraph.js
@@ -7,5 +7,5 @@ const Graph = ForceGraph3D()
       (document.getElementById('3d-graph'))
         .jsonUrl('../networkData-test.json')
         .nodeLabel('screen_name')
-        .nodeAutoColorBy('followers_count')
+        .nodeAutoColorBy('mean_bot_score')
         .onNodeClick(node => window.open(`https://twitter.com/${node.screen_name}`, '_blank'));
