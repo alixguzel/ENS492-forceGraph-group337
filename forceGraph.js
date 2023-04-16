af4203495e1781d63cbd95b758cd1a600b33db30
Ali Güzel <aliguzel.dev@gmail.com>
2023-04-16 18:44:58 +0300

    TestData

--- forceGraph.js
+++ forceGraph.js
@@ -5,7 +5,7 @@ console.log(inputValue);
 
 const Graph = ForceGraph3D()
       (document.getElementById('3d-graph'))
-        .jsonUrl('../networkData.json')
+        .jsonUrl('../networkData-test.json')
         .nodeLabel('screen_name')
         .nodeAutoColorBy('followers_count')
         .onNodeClick(node => window.open(`https://twitter.com/${node.screen_name}`, '_blank'));
