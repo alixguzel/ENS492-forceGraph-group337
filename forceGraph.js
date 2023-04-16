9c69bcd21696928102f172cf8c2b23b1ba29e7a2
Ali Güzel <aliguzel.dev@gmail.com>
2023-04-17 00:04:09 +0300

    Fixed reset button

--- forceGraph.js
+++ forceGraph.js
@@ -92,7 +92,6 @@ const resetBtn = document.getElementById("reset-btn");
 resetBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
-  const inputVal = document.getElementById("inputBox").value;
   Graph.graphData(gData);
 });
 
