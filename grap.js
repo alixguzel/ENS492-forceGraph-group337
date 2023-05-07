a48d0248a0a29162ad0f561d3b47e5e96b1c1f5e
hkadir24 <kadirucmazoglu1@gmail.com>
2023-05-07 16:54:01 +0300

    dynamic url

--- grap.js
+++ grap.js
@@ -1,8 +1,43 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const dataParams = urlParams.get('data');
+console.log(dataParams);
+console.log(typeof dataParams)
+
+
 async function fetchJson() {
-    let response = await fetch('./data.json');
+    if (dataParams == null){
+      let response = await fetch('./data.json');
+      let data = await response.json();
+      return data;
+    }
+    let response = await fetch('./'+dataParams);
     let data = await response.json();
     return data;
 }
+function createCard() {
+    // create card container
+    const card = document.createElement('div');
+    card.classList.add('card');
+  
+    // create row 1
+    const row1 = document.createElement('div');
+    row1.classList.add('row');
+    const row1Title = document.createElement('h3');
+    row1Title.innerText = 'Row 1';
+    const row1Text = document.createElement('p');
+    row1Text.innerText = 'This is the content of row 1';
+    row1.appendChild(row1Title);
+    row1.appendChild(row1Text);
+    card.appendChild(row1);
+    document.body.appendChild(card);
+    return card;
+}
+let card  = createCard();
+
+// add the card element to a container element in the DOM
+const container = document.getElementById('card-container');
+container.appendChild(card)
 
 const elem = document.getElementById('3d-graph');
 
@@ -13,7 +48,7 @@ const Graph = ForceGraph3D()(document.getElementById("3d-graph"))
   .nodeLabel("id")
   .nodeAutoColorBy("group")
   .graphData(gData)
-  .onNodeClick(node => window.open(`${node.user}/${node.id}`, '_blank'));
+  .onNodeClick(node => window.open(container));
 /*if(x == 6){
   Graph.onNodeClick(node => window.open(`${node.user}/${node.id}`, '_blank'));
 }
@@ -56,6 +91,5 @@ const resetBtn = document.getElementById("reset-btn");
 resetBtn.addEventListener("click", function(event) {
   event.preventDefault();
   
-  const inputVal = document.getElementById("inputBox").value;
   Graph.graphData(gData);
 });
