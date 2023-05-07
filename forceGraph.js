210a5e4d9b5b0f92c72c5da5fe83a2568b4c2588
Ali Güzel <aliguzel.dev@gmail.com>
2023-05-07 16:07:16 +0300

    Fixing the locations

--- forceGraph.js
+++ forceGraph.js
@@ -1,183 +1,13 @@
 let areNodesFiltered = false
 
 async function fetchJson() {
-  let response = await fetch('./politicianNodes_modified.json');
+  let response = await fetch('./politicianNodes_modified_final.json');
   let data = await response.json();
   return data;
 }
 
-
-function createFloatingBox(name, screen_name, num_followers, party) {
-  var floatingBox = document.querySelector('.floating-box-two');
-
-  if (floatingBox) {
-    floatingBox.remove();
-  }
-
-  const node = document.createElement("div");
-  node.classList.add("card");
-  node.classList.add("floating-box-two");
-  node.innerHTML = `
-      <h3 class="card-title">${name}</h3> 
-      <p><b>Number of Followers:</b> ${num_followers}</p>
-      <p><b>Political Party:</b> ${party}</p>
-      <p><b>Twitter:</b> <a href="https://twitter.com/${screen_name}" target="_blank">https://twitter.com/${screen_name}</a></p>
-      <button class="close-button">
-  <span>X</span>
-</button>
-    `;
-
-  document.body.appendChild(node);
-
-  const closeButton = node.querySelector('.close-button');
-  closeButton.addEventListener('click', function () {
-    node.remove();
-  });
-}
-
-function createPopUpBox(textInput) {
-  var floatingPopUpBox = document.querySelector('.floating-box-popUp');
-
-  if (floatingPopUpBox) {
-    floatingPopUpBox.remove();
-  }
-
-  const node = document.createElement("div");
-  node.classList.add("card");
-  node.classList.add("floating-box-popUp");
-  node.innerHTML = `
-      <p>${textInput}</p>
-    `;
-
-  document.body.appendChild(node);
-
-  const closeButton = node.querySelector('.close-button');
-  closeButton.addEventListener('click', function () {
-    node.remove();
-  });
-}
-
-// Function to filter nodes based on the input value
-function searchNodes(searchInputVal, createPopUp) {
-
-  let exactNode = gData.nodes.find((n) => n.screen_name == searchInputVal);
-
-  const distance = 40;
-  const distRatio = 1 + distance / Math.hypot(exactNode.x, exactNode.y, exactNode.z);
-
-  const newPos = exactNode.x || exactNode.y || exactNode.z
-    ? { x: exactNode.x * distRatio, y: exactNode.y * distRatio, z: exactNode.z * distRatio }
-    : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
-
-  Graph.cameraPosition(
-    newPos, // new position
-    exactNode, // lookAt ({ x, y, z })
-    3000  // ms transition duration
-  ); createFloatingBox(exactNode.name, exactNode.screen_name, exactNode.num_followers, exactNode.party)
-  if(createPopUp){createPopUpBox("Filtreler temizlendi")}
-}
-
-
-// Function to filter nodes based on the input value
-function filterNodes(partyinputVal, minFinputVal, maxFinputVal, party, minF, maxF) {
-
-
-  console.log(partyinputVal, minFinputVal, maxFinputVal, party, minF, maxF)
-
-  if (party) {
-    filteredNodes = filteredNodes.filter((n) => n.party == partyinputVal);
-  }
-  if (minF) {
-    filteredNodes = filteredNodes.filter((n) => n.num_followers > parseInt(minFinputVal));
-  }
-  if (maxF) {
-    filteredNodes = filteredNodes.filter((n) => n.num_followers < parseInt(maxFinputVal));
-  }
-
-  const filteredNodesIds = [];
-  JSON.stringify(filteredNodes, (key, value) => {
-    if (key === "id") filteredNodesIds.push(value);
-    return value;
-  });
-
-  const filteredLinks = gData.links.filter(
-    (e) =>
-      filteredNodesIds.includes(e.source.id) &&
-      filteredNodesIds.includes(e.target.id)
-  );
-
-  const filteredData = { nodes: filteredNodes, links: filteredLinks };
-
-  areNodesFiltered = true;
-  Graph.graphData(filteredData);
-}
-
-// Event listener for the filter input submit button
-const SearchSubmitBtn = document.getElementById("submit-search-btn");
-SearchSubmitBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  let createPopUp = false;
-
-  const searchInputVal = document.getElementById("search-inputBox").value;
-  const searchInputValPresent = gData.nodes.find(n => n.screen_name == searchInputVal);
-
-  if (searchInputVal != "" && searchInputValPresent != undefined) {
-
-    if (areNodesFiltered) {
-
-      const NodeAvailable = filteredNodes.find(n => n.screen_name == searchInputVal);
-
-      if (NodeAvailable) {
-
-      }
-      else {
-        console.log("Node not found")
-        createPopUp = true;
-        resetBtn.click();
-      }
-    }
-
-    searchNodes(searchInputVal, createPopUp);
-  }
-  else { createPopUpBox("Aradığınız kriterlere uygun sonuç bulunamadı"); }
-});
-
-
-// Event listener for the filter input submit button
-const submitBtn = document.getElementById("submit-btn");
-submitBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  let party, minF, maxF = false;
-
-  const partyinputVal = document.getElementById("party-inputBox").value;
-  const minFinputVal = document.getElementById("minF-inputBox").value;
-  const maxFinputVal = document.getElementById("maxF-inputBox").value;
-
-  if (partyinputVal != "") { party = true; }
-  if (minFinputVal != "") { minF = true; }
-  if (maxFinputVal != "") { maxF = true; }
-
-  filterNodes(partyinputVal, minFinputVal, maxFinputVal, party, minF, maxF);
-});
-
-// Event listener for removing the filters by clicking on the reset button
-const resetBtn = document.getElementById("reset-btn");
-resetBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  areNodesFiltered = false;
-  filteredNodes = gData.nodes;
-  Graph.graphData(gData);
-});
-
-
-const elem = document.getElementById('3d-graph');
 var gData = await fetchJson();
 var filteredNodes = gData.nodes
-console.log(typeof (filteredNodes))
-
 
 const Graph = ForceGraph3D()(document.getElementById("3d-graph"))
   .nodeLabel("screen_name")
@@ -201,11 +31,167 @@ const Graph = ForceGraph3D()(document.getElementById("3d-graph"))
 
 
 
-/*
-if(x == 6){
-Graph.onNodeClick(node => window.open(`${node.user}/${node.id}`, '_blank'));
-}
-else{
-Graph.nodeVisibility(node => node.group == 1);
-}
-*/
+  function createFloatingBox(name, screen_name, num_followers, party) {
+    var floatingBox = document.querySelector('.floating-box-two');
+  
+    if (floatingBox) {
+      floatingBox.remove();
+    }
+  
+    const node = document.createElement("div");
+    node.classList.add("card");
+    node.classList.add("floating-box-two");
+    node.innerHTML = `
+        <h3 class="card-title">${name}</h3> 
+        <p><b>Number of Followers:</b> ${num_followers}</p>
+        <p><b>Political Party:</b> ${party}</p>
+        <p><b>Twitter:</b> <a href="https://twitter.com/${screen_name}" target="_blank">https://twitter.com/${screen_name}</a></p>
+        <button class="close-button">
+    <span>X</span>
+  </button>
+      `;
+  
+    document.body.appendChild(node);
+  
+    const closeButton = node.querySelector('.close-button');
+    closeButton.addEventListener('click', function () {
+      node.remove();
+    });
+  }
+  
+  function createPopUpBox(textInput) {
+    var floatingPopUpBox = document.querySelector('.floating-box-popUp');
+  
+    if (floatingPopUpBox) {
+      floatingPopUpBox.remove();
+    }
+  
+    const node = document.createElement("div");
+    node.classList.add("card");
+    node.classList.add("floating-box-popUp");
+    node.innerHTML = `
+        <p>${textInput}</p>
+      `;
+  
+    document.body.appendChild(node);
+  
+    const closeButton = node.querySelector('.close-button');
+    closeButton.addEventListener('click', function () {
+      node.remove();
+    });
+  }
+  
+  // Function to filter nodes based on the input value
+  function searchNodes(searchInputVal, createPopUp) {
+  
+    let exactNode = gData.nodes.find((n) => n.screen_name == searchInputVal);
+  
+    const distance = 40;
+    const distRatio = 1 + distance / Math.hypot(exactNode.x, exactNode.y, exactNode.z);
+  
+    const newPos = exactNode.x || exactNode.y || exactNode.z
+      ? { x: exactNode.x * distRatio, y: exactNode.y * distRatio, z: exactNode.z * distRatio }
+      : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+  
+    Graph.cameraPosition(
+      newPos, // new position
+      exactNode, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    ); createFloatingBox(exactNode.name, exactNode.screen_name, exactNode.num_followers, exactNode.party)
+    if(createPopUp){createPopUpBox("Filtreler temizlendi")}
+  }
+  
+  
+  // Function to filter nodes based on the input value
+  function filterNodes(partyinputVal, minFinputVal, maxFinputVal, party, minF, maxF) {
+  
+  
+    console.log(partyinputVal, minFinputVal, maxFinputVal, party, minF, maxF)
+  
+    if (party) {
+      filteredNodes = filteredNodes.filter((n) => n.party == partyinputVal);
+    }
+    if (minF) {
+      filteredNodes = filteredNodes.filter((n) => n.num_followers > parseInt(minFinputVal));
+    }
+    if (maxF) {
+      filteredNodes = filteredNodes.filter((n) => n.num_followers < parseInt(maxFinputVal));
+    }
+  
+    const filteredNodesIds = [];
+    JSON.stringify(filteredNodes, (key, value) => {
+      if (key === "id") filteredNodesIds.push(value);
+      return value;
+    });
+  
+    const filteredLinks = gData.links.filter(
+      (e) =>
+        filteredNodesIds.includes(e.source.id) &&
+        filteredNodesIds.includes(e.target.id)
+    );
+  
+    const filteredData = { nodes: filteredNodes, links: filteredLinks };
+  
+    areNodesFiltered = true;
+    Graph.graphData(filteredData);
+  }
+  
+  // Event listener for the filter input submit button
+  const SearchSubmitBtn = document.getElementById("submit-search-btn");
+  SearchSubmitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+  
+    let createPopUp = false;
+  
+    const searchInputVal = document.getElementById("search-inputBox").value;
+    const searchInputValPresent = gData.nodes.find(n => n.screen_name == searchInputVal);
+  
+    if (searchInputVal != "" && searchInputValPresent != undefined) {
+  
+      if (areNodesFiltered) {
+  
+        const NodeAvailable = filteredNodes.find(n => n.screen_name == searchInputVal);
+  
+        if (NodeAvailable) {
+  
+        }
+        else {
+          console.log("Node not found")
+          createPopUp = true;
+          resetBtn.click();
+        }
+      }
+  
+      searchNodes(searchInputVal, createPopUp);
+    }
+    else { createPopUpBox("Aradığınız kriterlere uygun sonuç bulunamadı"); }
+  });
+  
+  
+  // Event listener for the filter input submit button
+  const submitBtn = document.getElementById("submit-btn");
+  submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+  
+    let party, minF, maxF = false;
+  
+    const partyinputVal = document.getElementById("party-inputBox").value;
+    const minFinputVal = document.getElementById("minF-inputBox").value;
+    const maxFinputVal = document.getElementById("maxF-inputBox").value;
+  
+    if (partyinputVal != "") { party = true; }
+    if (minFinputVal != "") { minF = true; }
+    if (maxFinputVal != "") { maxF = true; }
+  
+    filterNodes(partyinputVal, minFinputVal, maxFinputVal, party, minF, maxF);
+  });
+  
+  // Event listener for removing the filters by clicking on the reset button
+  const resetBtn = document.getElementById("reset-btn");
+  resetBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+  
+    areNodesFiltered = false;
+    filteredNodes = gData.nodes;
+    Graph.graphData(gData);
+  });
